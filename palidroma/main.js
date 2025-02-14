79d0813da35092bcc2538b4e5ab0786a587099b8
FabioMorrone <morronefabio04@gmail.com>
2025-02-14 15:24:53 +0100

    add

--- palidroma/main.js
+++ palidroma/main.js
@@ -2,7 +2,19 @@
  Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
 */
 
+// chiedere all'utente una parola // 
+const parola = prompt("testo")
+// creo funzione e passo la parola //
+function controlloParola(parola){
+ //meccanismo di controllo palindroma//
+ 
 
+  // ritorno del risultato// 
+} 
+ 
+
+// richiamo funzione e salvo risultato// 
+controlloParola(parola)
 
 
 
